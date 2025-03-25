daea766f2d3b613cb937915de5172f3f76428cd6
TatianaD258 <dumilieu.tatiana@gmail.com>
2025-03-25 17:14:26 +0100

    Update streamlit_app.py
    Show properties of smoothies

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,6 +50,9 @@ if ingredients_list :
 
     for fruit_chosen in ingredients_list :
         ingredients_string += fruit_chosen +' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        #st.text(smoothiefroot_response.json())
+        st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -68,7 +71,5 @@ if ingredients_list :
 
 #st.write(my_insert_stmt)
 
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    #st.text(smoothiefroot_response.json())
-    st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
