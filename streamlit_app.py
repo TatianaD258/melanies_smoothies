5ceb3332c5c4b4ce6883017bcdbca5a0f710db85
TatianaD258 <dumilieu.tatiana@gmail.com>
2025-03-26 13:10:36 +0100

    Update streamlit_app.py
    Use with SEARCH_ON values

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import pandas as pd
 from snowflake.snowpark.functions import col
 #from snowflake.snowpark.context import get_active_session
 
@@ -32,9 +33,13 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
 # Sans le select return all the table
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True) # show dataframe
+#st.stop()
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:'
                                  , my_dataframe
@@ -50,6 +55,7 @@ if ingredients_list :
 
     for fruit_chosen in ingredients_list :
         ingredients_string += fruit_chosen +' '
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         #st.text(smoothiefroot_response.json())
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
